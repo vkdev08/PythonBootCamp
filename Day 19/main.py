3862dc7574a6a9ad96ca67f91019d35560d775bf
vamsi <vamsi.dev08@gmail.com>
2025-08-22 19:51:46 +0530

    complete day22

--- Day 19/main.py
+++ Day 19/main.py
@@ -16,9 +16,9 @@ screen.onkey(move_forward, "w")
 screen.onkey(move_backward, "s")
 screen.onkey(move_left, "a")
 screen.onkey(key="d",fun=move_right) #here no () in move_forward
-#because its a method called inside a method when condition is triggered
+#because it's a method called inside a method when condition is triggered
 #parenthesis triggers it as soon the code reads it - which we don't want
-#funtion as a input into another function
+#funtion as an input into another function
 
 #Higher order functions:
 #a function as input into another functions - not many languages can do this
--- Day 22/ball.py
+++ Day 22/ball.py
@@ -10,6 +10,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
@@ -22,8 +23,9 @@ class Ball(Turtle):
     #collision with paddle
     def check_collision(self,paddle):
         if self.distance(paddle) < MIN_DISTANCE and (self.xcor()<LEFT_COLLISION or self.xcor() > RIGHT_COLLISION) :
-
             self.x_move *= -1
+            self.move_speed *= 0.9
 
     def reset_position(self):
+        self.move_speed = 0.1
         self.goto(0,0)
